62fbca377cf088b48ea88b280d3619bb657c1877
LaDarion Wells <ladarionwells@gmail.com>
2025-03-19 18:20:01 +0000

    Completed the DAO layer

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,110 @@
+package DAO;
+
+import Util.ConnectionUtil;
+import Model.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDAO {
+
+    //Create
+    public Account createAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO account(username, password) VALUES (?,?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+            
+            preparedStatement.executeUpdate();
+      
+            return account;
+        
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    //Read
+    public List<Account> getAllAccounts(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Account> accounts = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM account;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){ //change this if account-id is not needed
+                Account account = new Account(rs.getInt("account_id"),
+                rs.getString("username"),
+                rs.getString("password"));
+                accounts.add(account);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return accounts;
+    }
+
+    public Account getAccountById(int id){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE account_id = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            
+            preparedStatement.setInt(1, id);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()){ 
+                Account account = new Account(rs.getInt("account_id"),
+                rs.getString("username"),
+                rs.getString("password"));
+            return account;    
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    //Update
+    public void updateAccount(int id, Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "UPDATE account SET username = ?, password = ? WHERE account_id = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+            preparedStatement.setInt(3, id);
+           
+            preparedStatement.executeUpdate();
+
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Delete
+    public void deleteAccount(int id){ 
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "DELETE FROM account WHERE account_id = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, id);
+            
+            preparedStatement.executeUpdate();
+
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import Controller.SocialMediaController;
+import DAO.*;
+import Model.Account;
+import Model.Message;
+import Util.ConnectionUtil;
 import io.javalin.Javalin;
 
 /**
@@ -10,5 +17,57 @@ public class Main {
         SocialMediaController controller = new SocialMediaController();
         Javalin app = controller.startAPI();
         app.start(8080);
+
+
+        //THE FOLLOWING LINES OF CODE ARE TEST CODE
+
+        ConnectionUtil.getConnection();
+        ConnectionUtil.resetTestDatabase();
+        AccountDAO accountDAO = new AccountDAO();
+        MessageDAO messageDAO = new MessageDAO();
+
+        Account user1 = new Account( "ld", "123");
+        Account user2 = new Account(3, "well", "456");
+        Account user3 = new Account(4, "john", "12356");
+
+        Message message1 = new Message(1, "Hello World", 111119283);
+        Message message2 = new Message(2, "This is a test", 4355693);
+        Message message3 = new Message(1, "Today's gonna be a good day", 53295540);
+
+        //read
+        System.out.println("Initial array");
+        for(Message message : messageDAO.getAllMessages()){
+            System.out.println(message);
+        }
+        //create
+        Message test1 = messageDAO.createMessage(message1);
+        //should not work since there is no account with id 2
+        Message test2 = messageDAO.createMessage(message2);
+        System.out.println("After add");
+
+        for(Message message : messageDAO.getAllMessages()){
+            System.out.println(message);
+        }
+
+        System.out.println("Reattempt to add after Account with id 2 has been created");
+        Account add = accountDAO.createAccount(user1);
+        Message test3 = messageDAO.createMessage(message2);
+
+        for(Message message : messageDAO.getAllMessages()){
+            System.out.println(message);
+        }
+        //update
+        messageDAO.updateMessage(2, message3);
+        System.out.println("After update");
+        for(Message message : messageDAO.getAllMessages()){
+            System.out.println(message);
+        }
+        //delete
+        messageDAO.deleteMessage(1);
+        System.out.println("After delete");
+        for(Message message : messageDAO.getAllMessages()){
+            System.out.println(message);
+        }
+        
     }
 }
