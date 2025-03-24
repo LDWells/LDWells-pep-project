d801c70a5acdd391b7a1e6c8bc4508cbdcd96554
LaDarion Wells <ladarionwells@gmail.com>
2025-03-24 03:17:48 +0000

    Finished with AccountService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import Controller.SocialMediaController;
 import DAO.*;
 import Model.Account;
 import Model.Message;
+import Service.*;
 import Util.ConnectionUtil;
 import io.javalin.Javalin;
 
@@ -23,51 +24,45 @@ public class Main {
 
         ConnectionUtil.getConnection();
         ConnectionUtil.resetTestDatabase();
-        AccountDAO accountDAO = new AccountDAO();
-        MessageDAO messageDAO = new MessageDAO();
+
+        AccountService accountService = new AccountService();
 
         Account user1 = new Account( "ld", "123");
-        Account user2 = new Account(3, "well", "456");
-        Account user3 = new Account(4, "john", "12356");
+        Account user2 = new Account(3, "", "456");
+        Account user3 = new Account(4, "john", "");
+        Account user4 = new Account(7, "ladarion", "3134");
+        Account user5 = new Account( "wells", "564");
 
         Message message1 = new Message(1, "Hello World", 111119283);
         Message message2 = new Message(2, "This is a test", 4355693);
         Message message3 = new Message(1, "Today's gonna be a good day", 53295540);
 
         //read
-        System.out.println("Initial array");
-        for(Message message : messageDAO.getAllMessages()){
-            System.out.println(message);
-        }
-        //create
-        Message test1 = messageDAO.createMessage(message1);
-        //should not work since there is no account with id 2
-        Message test2 = messageDAO.createMessage(message2);
-        System.out.println("After add");
-
-        for(Message message : messageDAO.getAllMessages()){
-            System.out.println(message);
-        }
-
-        System.out.println("Reattempt to add after Account with id 2 has been created");
-        Account add = accountDAO.createAccount(user1);
-        Message test3 = messageDAO.createMessage(message2);
-
-        for(Message message : messageDAO.getAllMessages()){
-            System.out.println(message);
-        }
-        //update
-        messageDAO.updateMessage(2, message3);
-        System.out.println("After update");
-        for(Message message : messageDAO.getAllMessages()){
-            System.out.println(message);
-        }
-        //delete
-        messageDAO.deleteMessage(1);
-        System.out.println("After delete");
-        for(Message message : messageDAO.getAllMessages()){
-            System.out.println(message);
-        }
+        System.out.println(accountService.getAllAccounts());
         
+
+        //should not read
+        System.out.println(accountService.getAccountByID(5));
+
+        //create
+       accountService.createAccount(user1);
+       System.out.println("After create:");
+       System.out.println(accountService.getAllAccounts());
+
+        //should not create
+       accountService.createAccount(user2);
+        accountService.createAccount(user3);
+
+        //update
+        System.out.println(accountService.updateAccount(2, user5));
+        //should not update
+        System.out.println(accountService.updateAccount(2, user4));
+        System.out.println(accountService.updateAccount(110, user5));
+
+
+        //delete
+        System.out.println(accountService.deleteAccount(2));
+        //should not delete
+        System.out.println(accountService.deleteAccount(120));
     }
 }
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,73 @@
 package Service;
 
+import DAO.AccountDAO;
+import Model.Account;
+import java.util.*;
+
 public class AccountService {
+    AccountDAO accountDAO;
+
+
+    public AccountService(){
+        accountDAO = new AccountDAO(); //dependency injection
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    //create
+    public void createAccount(Account account){
+        if(account.getUsername() == ""){
+            System.out.println("You must enter a username!");
+            //return null;
+        }
+        else if(account.getPassword() == ""){
+            System.out.println("You must enter a password!");
+            //return null;
+        }
+        else{
+            System.out.println("Account created!");
+            accountDAO.createAccount(account); 
+        }
+    } 
+
+    //read
+    public List<Account> getAllAccounts(){
+        return accountDAO.getAllAccounts();
+    }
+
+    public Account getAccountByID(int id){
+        if(accountDAO.getAccountById(id) == null){
+            System.out.println("Account ID " + id + " does not exist.");
+            return null;
+        }
+        return accountDAO.getAccountById(id);
+    }
+
+    //update
+    public boolean updateAccount(int id, Account account){
+        if(accountDAO.getAccountById(id) == null){
+            System.out.println("Account ID " + id + " does not exist.");
+            return false;
+        }
+        else{
+            accountDAO.updateAccount(id, account);
+            account.setAccount_id(id);
+            return true;
+        }
+    }
+    //delete
+    public boolean deleteAccount(int id){
+        if(accountDAO.getAccountById(id) != null){
+            accountDAO.deleteAccount(id);
+            System.out.println("Account ID " + id + " has been deleted!");
+            return true;
+        }
+        else{
+            System.out.println("Account ID " + id + " does not exist.");
+            return false;
+        }
+    }
     
 }
